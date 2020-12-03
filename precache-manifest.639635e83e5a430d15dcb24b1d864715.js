8e566958032240eae62c4a8c771c5af53b5f0533
Abhishek <a.a.mandilkar@gmail.com>
2020-12-03 23:54:52 +0530

    Updates

---
+++ precache-manifest.639635e83e5a430d15dcb24b1d864715.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812e5fd56ddcf34e933d4a082887888c",
+    "url": "/Note-It/index.html"
+  },
+  {
+    "revision": "e2d34ace1bd6a74886fd",
+    "url": "/Note-It/static/js/2.65246666.chunk.js"
+  },
+  {
+    "revision": "d9556cef6cbca680ece9",
+    "url": "/Note-It/static/js/main.50a45f2c.chunk.js"
+  },
+  {
+    "revision": "60a8d6008ce079eb7183",
+    "url": "/Note-It/static/js/runtime-main.9cd2f3cc.js"
+  }
+]);
